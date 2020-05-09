0f96c96438a372b86b790746781227755bac7bee
Kyle McLean <kyledmclean@gmail.com>
2020-05-09 23:05:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+require `pry`
+class Dog 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all.push self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    binding.pry
+    self.class_variable_get
+    @@all.each do |name|
+      puts name
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
